50feb8cd2d61a1bf50436729c19da2912408b1c4
Kirya052 <kiryakonev01@mail.ru>
2022-09-19 19:48:56 +0300

    Initialize project

---
+++ TankoGeddon/Source/TankoGeddon/TankPawn.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPawn.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ATankPawn::ATankPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	RootComponent = BoxCollision;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	BodyMesh->SetupAttachment(BoxCollision);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(BodyMesh);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(BoxCollision);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATankPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATankPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//MoveForward
+	FVector currentLocation = GetActorLocation();
+	FVector ForwardVector = GetActorForwardVector();
+	UE_LOG(LogTemp, Warning, TEXT("ForwardVector: %s"), *ForwardVector.ToString());
+	FVector RightVector = GetActorRightVector();
+	UE_LOG(LogTemp, Warning, TEXT("RightVector: %s"), *RightVector.ToString());
+	FVector UpVector = GetActorUpVector();
+	UE_LOG(LogTemp, Warning, TEXT("UpVector: %s"), *UpVector.ToString());
+	FVector movePosition = currentLocation + ForwardVector * MovementSpeed * ForwardMoveAxisValue * DeltaTime;
+	SetActorLocation(movePosition);
+}
+
+void ATankPawn::MoveForward(float Value)
+{
+	ForwardMoveAxisValue = Value;
+}
+
