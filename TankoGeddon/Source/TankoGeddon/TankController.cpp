ddfa4adfb57363c5bc3198f5788d4cf61d00056a
Kirya052 <kiryakonev01@mail.ru>
2022-10-05 22:38:24 +0300

    Update

--- TankoGeddon/Source/TankoGeddon/TankController.cpp
+++ TankoGeddon/Source/TankoGeddon/TankController.cpp
@@ -3,6 +3,12 @@
 
 #include "TankController.h"
 #include "TankPawn.h"
+#include "DrawDebugHelpers.h"
+
+ATankController::ATankController()
+{
+	bShowMouseCursor = true;
+}
 
 void ATankController::SetPawn(APawn* InPawn)
 {
@@ -14,6 +20,27 @@ void ATankController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
 	InputComponent->BindAxis("MoveForward", this, &ATankController::MoveForward);
+	InputComponent->BindAxis("RotateRight", this, &ATankController::RotateRight);
+	InputComponent->BindAction("Fire", EInputEvent::IE_Pressed, this, &ATankController::Fire);
+	InputComponent->BindAction("FireSpecial", EInputEvent::IE_Pressed, this, &ATankController::FireSpecial);
+	InputComponent->BindAction("ChangeCannon", EInputEvent::IE_Pressed, this, &ATankController::ChangeCannon);
+}
+
+void ATankController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	FVector MouseDirection;
+	DeprojectMousePositionToWorld(MousePos, MouseDirection);
+	if (TankPawn)
+	{
+		FVector tankPosition = TankPawn->GetActorLocation();
+		MousePos.Z = tankPosition.Z;
+		FVector dir = MousePos - tankPosition;
+		dir.Normalize();
+		MousePos = tankPosition + dir * 1000;
+	}
+	//DrawDebugLine(GetWorld(), tankPosition, MousePos, FColor::Green, false, 1.0f, 0, 5);
 }
 
 void ATankController::MoveForward(float Value)
@@ -21,5 +48,37 @@ void ATankController::MoveForward(float Value)
 	if (TankPawn)
 	{
 		TankPawn->MoveForward(Value);
+	}
+}
+
+void ATankController::RotateRight(float Value)
+{
+	if (TankPawn)
+	{
+		TankPawn->RotateRight(Value);
+	}
+}
+
+void ATankController::Fire()
+{
+	if (TankPawn)
+	{
+		TankPawn->Fire();
+	}
+}
+
+void ATankController::FireSpecial()
+{
+	if (TankPawn)
+	{
+		TankPawn->FireSpecial();
+	}
+}
+
+void ATankController::ChangeCannon()
+{
+	if (TankPawn)
+	{
+		TankPawn->ChangeCannon();
 	}
 }
